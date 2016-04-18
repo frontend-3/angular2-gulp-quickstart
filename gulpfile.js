5346e6255bfb0605fa448ebaa8c09372ce2b5438
Elizabeth Manrique <ecmanrique@gmail.com>
2016-04-18 14:47:52 -0500

    fix(gulpfile): deleted reference to dev/prod enviroments

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,6 @@ gulp = require('gulp');
 config = require('./config.js');
 
 require('gulp-simple-load-tasks')(gulp)
-settings = require('./settings/dev');
 
-config.settings = settings;
 gulp.config = config;
 gulp.loadTasks(__dirname + '/tasks');
