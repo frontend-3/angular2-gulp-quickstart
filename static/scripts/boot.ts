e4498db1c3cb513c4f2e9a6ed9d6a2587007d548
Elizabeth Manrique <ecmanrique@gmail.com>
2016-04-19 12:27:49 -0500

    fix(scripts): change modules to componets

--- static/scripts/boot.ts
+++ static/scripts/boot.ts
@@ -1,6 +1,6 @@
 ///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
 
 import {bootstrap} from 'angular2/platform/browser';
-import {AppComponent} from './modules/app.component';
+import {AppComponent} from './components/app.component';
 
 bootstrap(AppComponent);
